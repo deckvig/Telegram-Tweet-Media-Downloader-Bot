57837351037afbe3d2eece44fc2d2496ef0bef58
Deckvig <2008.zhengxing@gmail.com>
2024-07-23 16:39:17 +0000

    update

--- src/bot.py
+++ src/bot.py
@@ -1,96 +1,84 @@
-import time, sys, requests, random, json, logging, subprocess, os, urllib3
+import time, sys, httpx, random, json, logging, subprocess, os
 from tMsgSender import tMsgSender
 from tMsgFetcher import tMsgFetcher, messageInfo
 from tMsgText import tMsgText
 from config import Config
 
-
 class bot:
     def __init__(self):
-        self.msgOffset: int = 0
-        self.pollTimeout: int = 50
+        self.msgOffset = 0
+        self.pollTimeout = 50
         logging.info(f"Using max long polling timeout of {self.pollTimeout} seconds")
 
     def getHelp(self):
-        # print help information, then quit
-        logging.info("\nList of options:\n\n"+
-            "(t)oken to use for telegram bot API [token]\n")
+        logging.info("\nList of options:\n\n(t)oken to use for telegram bot API [token]\n")
         sys.exit(0)
 
     def handleMessage(self, msg):
         logging.info("Handling message(s)")
         tMsgText(msg['message'], self.sender, conf).process()
-        self.msgOffset = msg['update_id'] + 1  # Update msg offset
+        self.msgOffset = msg['update_id'] + 1
         logging.info(f"Message offset updated to {self.msgOffset}")
-    
+
     def verifyAPIToken(self):
+        logging.info("Using proxies from environment variables")  # Log information about used proxies
         proxies = {
-           'http': os.environ.get("HTTP_PROXY"),
-           'https': os.environ.get("HTTPS_PROXY"),
+            'http://': os.environ.get("HTTP_PROXY"),
+            'https://': os.environ.get("HTTPS_PROXY"),
         }
-        logging.info(f"Using proxies: {proxies}")  # Log information about used proxies
+        logging.info("Attempting to verify Telegram API token")
         try:
-            logging.info("Attempting to verify Telegram API token")
-            # connect to Telegram API with their getMe test method for checking API works
-            testResponse: requests.Response = requests.get(f"https://api.telegram.org/bot{conf.tToken}/getMe",verify=False, proxies=proxies)
-            # set the token to be used if we get a 2xx response code back
-            match testResponse.ok:
-                case True:
-                    self.bottoken: str = conf.tToken
+            with httpx.Client(proxies=proxies, verify=False) as client:
+                testResponse = client.get(f"https://api.telegram.org/bot{conf.tToken}/getMe")
+                if testResponse.status_code == 200:
+                    self.bottoken = conf.tToken
                     logging.info("Telegram API token OK")
-                case _:
+                else:
                     logging.error("Telegram API check failed")
                     self.getHelp()
         except Exception as ex:
             logging.error("Telegram API token verification failed", exc_info=ex)
             self.getHelp()
-    
+
     def createSenderFetcher(self):
-        self.sender: tMsgSender = tMsgSender(self.bottoken)
-        self.fetcher: tMsgFetcher = tMsgFetcher(self.bottoken, self.pollTimeout)
-    
+        self.sender = tMsgSender(self.bottoken)
+        self.fetcher = tMsgFetcher(self.bottoken, self.pollTimeout)
+
     def getBotInfo(self):
         logging.info("Getting Bot info from Telegram")
         response = self.sender.sendGetMe()
         if not response.content:
             logging.error("Empty response received from Telegram")
-            # Handle empty response here (e.g., retry)
             return
         self.botInfo = json.loads(response.content)['result']
         self.bot_id = self.botInfo['id']
         self.bot_username = self.botInfo['username']
         logging.info(f"Got bot info - ID: {self.bot_id}, username: {self.bot_username}")
-    
+
     def run(self):
         self.createSenderFetcher()
         self.getBotInfo()
         while True:
-            # fetch all the new messages from Telegram servers
             logging.info("Sending off to wait for new data")
-            response: messageInfo = self.fetcher.fetchMessages(self.msgOffset)
+            response = self.fetcher.fetchMessages(self.msgOffset)
             logging.info("Received new Telegram data")
-            match response.tResponseOk:
-                case True:
-                    logging.info("Telegram response was OK")
-                    list(map(lambda x: self.handleMessage(x), response.tResult))
-                case _:
-                    logging.warning(f"Telegram response indicated error! {response.errCode} - {response.errDesc}")
-                    # failed to fetch new messages, wait for random number of seconds then try again
-                    sleepTime: int = random.randint(self.pollTimeout, self.pollTimeout*2)
-                    logging.info(f"Sleeping for {sleepTime} seconds")
-                    time.sleep(sleepTime)
-
+            if response.tResponseOk:
+                logging.info("Telegram response was OK")
+                list(map(self.handleMessage, response.tResult))
+            else:
+                logging.warning(f"Telegram response indicated error! {response.errCode} - {response.errDesc}")
+                sleepTime = random.randint(self.pollTimeout, self.pollTimeout * 2)
+                logging.info(f"Sleeping for {sleepTime} seconds")
+                time.sleep(sleepTime)
 
 if __name__ == '__main__':
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     print("Loading configuration")
-    conf: Config = Config()
+    conf = Config()
     logging.basicConfig(format='%(asctime)s %(levelname)s - %(message)s', level=conf.logLevel)
     conf.loadEnvVars()
     galleryDlVersion = subprocess.run(["gallery-dl", "--version"], capture_output=True)
-    logging.info(f"Using gallery-dl version: {galleryDlVersion.stdout}")
+    logging.info(f"Using gallery-dl version: {galleryDlVersion.stdout.decode().strip()}")
 
-    # Telegram Polling Configuration
-    tBot: bot = bot()
+    tBot = bot()
     tBot.verifyAPIToken()
     tBot.run()
--- src/tMsgSender.py
+++ src/tMsgSender.py
@@ -1,4 +1,4 @@
-import requests, logging, os
+import httpx, logging, os
 
 class recievedData:
     def __init__(self, isOk: bool, isErr: bool=False, statusCode: int=-1, content: bytes=bytearray(0), errDetails: str=""):
@@ -16,18 +16,14 @@ class tMsgSender:
     def generateRequest(self, msgParams: list) -> str:
         logging.debug("Generating request string")
 
-        match msgParams:
-            # if there's multiple parameters, have to append them correctly
-            case p if len(msgParams) > 3:
-                requestString = f"{self.tAPIUrl}/{str(p[0])}?"
-                # skip the 0th item, already appended it to the requestString
-                for i in range(1, len(p)-3, 2):
-                    requestString = f"{requestString}{str(p[i])}={str(p[i+1])}&"
-                requestString = f"{requestString}{str(p[-2])}={str(p[-1])}"
-            case p if len(msgParams) > 1:
-                requestString = f"{self.tAPIUrl}/{str(p[0])}?{str(p[1])}={str(p[2])}"
-            case p:
-                requestString = f"{self.tAPIUrl}/{str(p[0])}"
+        if len(msgParams) > 3:
+            requestString = f"{self.tAPIUrl}/{msgParams[0]}?"
+            requestString += '&'.join([f"{msgParams[i]}={msgParams[i+1]}" for i in range(1, len(msgParams) - 1, 2)])
+        elif len(msgParams) > 1:
+            requestString = f"{self.tAPIUrl}/{msgParams[0]}?{msgParams[1]}={msgParams[2]}"
+        else:
+            requestString = f"{self.tAPIUrl}/{msgParams[0]}"
+
         logging.debug(f"Generated request string: {requestString}")
         return requestString
 
@@ -43,19 +39,16 @@ class tMsgSender:
     def sendSilentMessage(self, text: str, chat_id: str) -> recievedData:
         return self.sendRequest(["sendMessage", "chat_id", chat_id, "text", text, "disable_web_page_preview", True, "disable_notification", True])
 
-
     def sendRequest(self, msgParams: list) -> recievedData:
         requestString = self.generateRequest(msgParams)
         proxies = {
-           'http': os.environ.get("HTTP_PROXY"),
-           'https': os.environ.get("HTTPS_PROXY")
+            'http://': os.environ.get("HTTP_PROXY"),
+            'https://': os.environ.get("HTTPS_PROXY")
         }
         try:
-            # Use both a connect and read timeout. Should establish a connection
-            # to Telegram within the connect timeout, but shouldn't consider the
-            # connection to have been broken until after the long polling duration
-            request: requests.Response = requests.get(requestString, timeout=(5, 60),verify=False, proxies=proxies)
-            # return True/False for a status code of 2XX, the status code itself and the response content
-            return recievedData(request.ok, statusCode=request.status_code, content=request.content)
+            with httpx.Client(proxies=proxies, verify=False) as client:
+                response = client.get(requestString, timeout=(5, 60))
+                return recievedData(response.status_code == 200, statusCode=response.status_code, content=response.content)
         except Exception as e:
-            return recievedData(isOk=False, isErr=True, errDetails=f"Error making request {requestString}, {str(e)}" )
+            logging.error(f"Error making request {requestString}: {str(e)}")
+            return recievedData(isOk=False, isErr=True, errDetails=f"Error making request {requestString}: {str(e)}")
